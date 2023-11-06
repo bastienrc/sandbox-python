d31fe90f0021113dd0e0cf09e2ba57ca914b639a
Sebastien Colbert <colbertsebastien@gmail.com>
2023-11-06 13:17:43 +0000

    👌 IMPROVE: Casse-briques, la raquettes

--- games/casse-briques/main.py
+++ games/casse-briques/main.py
@@ -2,8 +2,21 @@ import pygame
 
 config = {
     "screen_width":  600,
-    "screen_height": 600
+    "screen_height": 600,
+    "racket_width":   80,
+    "racket_height":  10
 }
+
+
+class Racket(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.surf = pygame.Surface(
+            [config["racket_width"], config["racket_height"]])
+        self.surf.fill((0, 255, 0))
+        self.rect = self.surf.get_rect()
+        self.rect.x = config["screen_width"] / 2 - config["racket_width"] / 2
+        self.rect.y = config["screen_height"] - 2 * config["racket_height"]
 
 
 pygame.init()
@@ -12,6 +25,10 @@ screen = pygame.display.set_mode(
     [config["screen_width"], config["screen_height"]])
 running = True
 
+all_sprites = pygame.sprite.Group()
+my_racket = Racket()
+all_sprites.add(my_racket)
+
 
 while running:
     for event in pygame.event.get():
@@ -19,6 +36,9 @@ while running:
             running = False
 
     screen.fill("black")
+
+    for my_sprite in all_sprites:
+        screen.blit(my_sprite.surf, my_sprite.rect)
 
     pygame.display.flip()
 
