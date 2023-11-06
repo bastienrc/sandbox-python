81e7da86aba3b24550e3625a65e11d26a96f722e
Sebastien Colbert <colbertsebastien@gmail.com>
2023-11-06 14:12:33 +0000

    👌 IMPROVE: Casse-briques, déplacement de la raquette

--- games/casse-briques/main.py
+++ games/casse-briques/main.py
@@ -4,7 +4,8 @@ config = {
     "screen_width":  600,
     "screen_height": 600,
     "racket_width":   80,
-    "racket_height":  10
+    "racket_height":  10,
+    "step_move_racket": 25
 }
 
 
@@ -18,11 +19,22 @@ class Racket(pygame.sprite.Sprite):
         self.rect.x = config["screen_width"] / 2 - config["racket_width"] / 2
         self.rect.y = config["screen_height"] - 2 * config["racket_height"]
 
+    def update(self, pressed_keys):
+        if pressed_keys[pygame.K_LEFT]:
+            self.rect.move_ip(- config["step_move_racket"], 0)
+        if pressed_keys[pygame.K_RIGHT]:
+            self.rect.move_ip(config["step_move_racket"], 0)
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > config["screen_width"]:
+            self.rect.right = config["screen_width"]
+
 
 pygame.init()
 pygame.display.set_caption("Casse Briques Alpha-0.0.1")
 screen = pygame.display.set_mode(
     [config["screen_width"], config["screen_height"]])
+clock = pygame.time.Clock()
 running = True
 
 all_sprites = pygame.sprite.Group()
@@ -37,9 +49,16 @@ while running:
 
     screen.fill("black")
 
+    keys = pygame.key.get_pressed()
+
+    for my_sprite in all_sprites:
+        my_sprite.update(keys)
+
     for my_sprite in all_sprites:
         screen.blit(my_sprite.surf, my_sprite.rect)
 
     pygame.display.flip()
+
+    clock.tick(30)
 
 pygame.quit()
